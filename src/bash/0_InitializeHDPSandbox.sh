f11237ec27c250bdac3a1aa47567efe7004177be
Boris Reynard <boris.reynard@semsoft-corp.com>
2016-01-29 17:40:25 +0100

    Demo With Sqoop

--- src/bash/0_InitializeHDPSandbox.sh
+++ src/bash/0_InitializeHDPSandbox.sh
@@ -6,7 +6,12 @@ source conf.sh
 echo "";
 echo "Initialize Atlas in the HDP Sandbox";
 
-#git clone https://github.com/sequenceiq/docker-ambari.git
+echo ""
+echo "Get Docker Ambari"
+rm -rf /tmp/dockerAmbari
+mkdir /tmp/dockerAmbari
+cd /tmp/dockerAmbari
+git clone https://github.com/sequenceiq/docker-ambari.git
 
 docker stop amb1
 docker stop amb2
@@ -22,9 +27,12 @@ docker rm amb-consul
 #blueprint add --url https://raw.githubusercontent.com/sequenceiq/ambari-rest-client/2.2.0/src/main/resources/blueprints/multi-node-hive
 
 
-cd xxxx
+
+echo ""
+echo "Start docker Ambari"
+cd docker-ambari
 source ambari-functions
-amb-settings
+#amb-settings
 #amb-deploy-cluster 3 multi-node-hive
 amb-deploy-cluster 1 single-node-hdfs-yarn
 
@@ -32,8 +40,14 @@ amb-deploy-cluster 1 single-node-hdfs-yarn
 amb-settings
 # Go to http://172.17.0.3:8080/ admin/admin
 
+echo "";
+echo "Fin";
 
-#Modification import-hive.sh lectur /bridge/hive et /hook/hive (cf https://issues.apache.org/jira/browse/ATLAS-92)
+echo ""
+echo "Modification hive-import ..."
+
+return
+#Modification import-hive.sh lecture /bridge/hive et /hook/hive (cf https://issues.apache.org/jira/browse/ATLAS-92)
 ssh ${SSH_USER_NAME}@${SERVER_NAME} "wget https://issues.apache.org/jira/secure/attachment/12748786/ATLAS-92.000.patch /tmp/ATLAS-92.000.patch";
 ssh ${SSH_USER_NAME}@${SERVER_NAME} "cp /usr/hdp/current/atlas-server/bin/import-hive.sh /usr/hdp/current/atlas-server/bin/import-hive.sh.orig";
 ssh ${SSH_USER_NAME}@${SERVER_NAME} "patch /usr/hdp/current/atlas-server/bin/import-hive.sh /tmp/ATLAS-92.000.patch";
